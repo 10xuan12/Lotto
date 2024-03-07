545e5c641e2673c8a210e1f9535ee837271361e1
xuan <s1114670@pu.edu.tw>
2024-03-07 22:40:06 +0800

    第二次 commit

--- app/src/main/java/tw/edu/pu/s1114670/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1114670/lotto/MainActivity.kt
@@ -2,10 +2,24 @@ package tw.edu.pu.s1114670.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = "0"
     }
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        if (v.id == R.id.btn){
+            txv.text = (1..100).random().toString()
+        }
+        else{
+            txv.text = "0"
+        }
+    }
+
 }
